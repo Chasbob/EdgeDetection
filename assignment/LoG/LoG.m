b22f0c24b2b8e55c83ca7a528c3ff00f77e6c347
Charles de Freitas <ypph279@gmail.com>
2018-11-23 14:39:58 +0000

    LoG

---
+++ assignment/LoG/LoG.m
@@ -0,0 +1,10 @@
+function output = LoG(image)
+    n = 8;
+    sd = 1;
+    gaus1d = N(0, sd, -n:1:n);
+    gaus2d = gaus1d' * gaus1d;
+    laplacian = [[0 1 0]' [1 (-4) 1]' [0 1 0]'];
+    LoG_temp = conv2(laplacian, gaus2d, 'same');
+    LoG_image = conv2(image, LoG_temp, 'same');
+    output = edge(LoG_image, 'zerocross');
+end
