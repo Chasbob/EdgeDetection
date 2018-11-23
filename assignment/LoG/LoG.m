edc29cc93b59a1483192098a8d497c98c51db16c
Charles de Freitas <ypph279@gmail.com>
2018-11-23 15:55:12 +0000

    Changed so that laplacian is convolved onto gaussian

--- assignment/LoG/LoG.m
+++ assignment/LoG/LoG.m
@@ -4,7 +4,7 @@ function output = LoG(image)
     gaus1d = N(0, sd, -n:1:n);
     gaus2d = gaus1d' * gaus1d;
     laplacian = [[0 1 0]' [1 (-4) 1]' [0 1 0]'];
-    LoG_temp = conv2(laplacian, gaus2d, 'same');
+    LoG_temp = conv2(gaus2d, laplacian, 'same');
     LoG_image = conv2(image, LoG_temp, 'same');
     output = edge(LoG_image, 'zerocross');
 end
